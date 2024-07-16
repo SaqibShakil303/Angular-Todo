c59dd529cc284200aa5cfedc7d98989870e663a9
freak303 <mds168723@gmail.com>
2024-07-16 22:50:58 +0530

    Route&components config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { ViewTodosComponent } from './pages/view-todos/view-todos.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ {
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+ }, {
+    path:'home',
+    component: HomeComponent
+  }
+  ,{
+    path:'add-todo',
+    component: AddTodoComponent
+  }
+  ,{
+   path:'view-todo',
+   component: ViewTodosComponent
+  }
+  ,{
+    path:'update-todo',
+    component: UpdateTodoComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
